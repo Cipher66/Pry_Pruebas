4896c96ab9e65abcc4eeddd4823c79a85a753a8a
Cipher66 <schavezfe16dw@ikzubirimanteo.com>
2017-11-20 06:01:03 +0100

    Initial commit

---
+++ resources/views/formGET.blade.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>Formulario con GET</title>
+    </head>
+    <body>
+        <form method="GET" action="/">
+            Nombre: <input type="text" name="nombre" />
+            Apellido: <input type="text" name="apellido" />
+            <input type="submit" value="Enviar" />
+        </form>
+    </body>
+</html>
---
+++ resources/views/formPOST.blade.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title>Formulario con post</title>
+    </head>
+    <body>
+        <form method="POST">
+            Nombre: <input type="text" name="nombre">
+            Apellido: <input type="text" name="apellido">
+        </form>
+    </body>
+</html>
